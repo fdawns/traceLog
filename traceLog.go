32e8c169c1c0456b0551457abf2cd098b31aab9e
dawnsink <47072844+fdawns@users.noreply.github.com>
2025-11-05 12:21:38 +0800

    Update traceLog.go

--- traceLog.go
+++ traceLog.go
@@ -4,51 +4,33 @@ import (
 	"bytes"
 	"context"
 	"runtime"
-	"strconv"
 	"sync"
-	"unsafe"
 )
 
-var contextMap sync.Map
+var contextMap = sync.Map{}
 
-var gidCache sync.Map
-
-func getGID() uint64 {
-	var x int
-	ptr := unsafe.Pointer(&x)
-	
-	if v, ok := gidCache.Load(ptr); ok {
-		return v.(uint64)
-	}
-	
+func getGoroutineID() string {
 	var buf [64]byte
 	n := runtime.Stack(buf[:], false)
-	fields := bytes.Fields(buf[:n])
-	if len(fields) < 2 {
-		return 0
-	}
-	id, _ := strconv.ParseUint(string(fields[1]), 10, 64)
-	
-	gidCache.Store(ptr, id)
-	return id
-}
-
-func SetGoroutineContext(ctx context.Context) {
-	id := getGID()
-	contextMap.Store(id, ctx)
+	return string(bytes.Fields(buf[:n])[1])
 }
 
 func GetGoroutineContext() context.Context {
-	id := getGID()
-	if v, ok := contextMap.Load(id); ok {
-		if ctx, ok := v.(context.Context); ok {
-			return ctx
-		}
+	value, ok := contextMap.Load(getGoroutineID())
+	if !ok {
+		return context.Background()
 	}
-	return context.Background()
+	ctx, ok := value.(context.Context)
+	if !ok {
+		return context.Background()
+	}
+	return ctx
+}
+
+func SetGoroutineContext(ctx context.Context) {
+	contextMap.Store(getGoroutineID(), ctx)
 }
 
 func DelGoroutineContext() {
-	id := getGID()
-	contextMap.Delete(id)
+	contextMap.Delete(getGoroutineID())
 }
